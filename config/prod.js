b4eab0334d11e75d414842e29b14022c066bb932
august.nejudne <august.nejudne@gmail.com>
2018-10-18 18:42:51 +0800

    finished auth flow

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,16 @@
-// prod.js - commit this
+// prod.js - production keys here
 module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+/*
   googleClientID:
     '711378623447-4rfrdm3luuq1v9ck5c4alu4gr4404f09.apps.googleusercontent.com',
   googleClientSecret: '1faZiLsSw426MsZHxBQzqK99',
   mongoURI:
     'mongodb://august.nejudne:pringles1432@ds119663.mlab.com:19663/emaily-dev',
   cookieKey: 'thequickbrownfoxjumpsoverthelazydog'
-};
+
+*/
